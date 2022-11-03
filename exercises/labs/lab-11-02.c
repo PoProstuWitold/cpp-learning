c31f13fbf926d3f54176ecfb441e810fd98f8a41
PoProstuWitold <witekzawada@o2.pl>
2022-11-03 16:11:38 +0100

    lab 11 02 finished

--- exercises/labs/lab-11-02.c
+++ exercises/labs/lab-11-02.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <math.h>
-#include <ctype.h>
 
 #define AUTHOR "Witold Zawada"
 #define STUDY_INFO "informatyka I stopnia (stacjonarne)/semestr 1"
@@ -8,59 +7,73 @@
 #define PROGRAM_NAME "for loops"
 
 void print_author_info(void);
+int insert_number(int num, int num1, int num2);
+void empty_stdin(void);
+
+struct size_sum {
+    int size;
+    double sum;
+};
+
 
 int main() {
 
-    int num, size;
-    int size_neg=0, size_1_10=0, size_every_second=0, size_even=0, size_odd=0;
-    double sum_sqrt=0.0, sum_neg=0, sum_1_10=0.0, sum_odd=0.0, sum_even=0.0, product_every_sec=1.0;
+    int num;
 
+    struct size_sum basic = { 0, 0.0 };
+    struct size_sum negative = { 0, 0.0 };
+    struct size_sum basic_1_10 = { 0, 0.0 };
+    struct size_sum every_second = { 0, 1.0 };
+    struct size_sum even = { 0, 0.0 };
+    struct size_sum odd = { 0, 0.0 };
 
     print_author_info();
+    printf("-----------------------------\n");
+    basic.size = insert_number(num, 0, 0);
+    printf("Enter %d numbers: \n", basic.size);
 
-    printf("Enter the value of n: ");
-    // TO DO: Input validation
-    scanf("%d", &size);
-    printf("Enter %d numbers: \n", size);
+    for(int i=0; i<basic.size; i++) {
+        // 1. obliczyć i wyświetlić sumę kwadratów liczb
+        num = insert_number(num, i+1, basic.size);
+        basic.sum = basic.sum + pow(num, 2);
 
-    for(int i=0; i<size; i++) {
-        printf("Enter %d of %d number: ", i+1, size);
-        scanf("%d", &num);
-
-        sum_sqrt = sum_sqrt + pow(num, 2);
-
+        // 2. obliczyć i wyświetlić średnią arytmetyczną liczb ujemnych
         if(num < 0) {
-            size_neg++;
-            sum_neg = sum_neg+num;
+            negative.size++;
+            negative.sum = negative.sum+num;
         }
 
+        // 3. obliczyć i wyświetlić sumę liczb z przedziału <1,10>
         if(num >= 1 && num <= 10) {
-            size_1_10++;
-            sum_1_10 = sum_1_10 + num;
+            basic_1_10.size++;
+            basic_1_10.sum = basic_1_10.sum + num;
         }
 
+        // 4. obliczyć i wyświetlić iloczyn co drugiej liczby 
+        // (zaczynamy od pierwszej przeczytanej)
         if(i % 2 == 0 || i == 0) {
-            size_every_second++;
-            product_every_sec = num * product_every_sec;
+            every_second.size++;
+            every_second.sum = num * every_second.sum;
         }
 
+        // 5. obliczyć i wyświetlić sumę liczb parzystych i sumę nieparzystych
         if(num % 2 == 0) {
-            size_even++;
-            sum_even = sum_even + num;
+            even.size++;
+            even.sum = even.sum + num;
         }
-
+        // 5. obliczyć i wyświetlić sumę liczb parzystych i sumę nieparzystych
         if(!(num % 2 == 0)) {
-            size_odd++;
-            sum_odd = sum_odd + num;
+            odd.size++;
+            odd.sum = odd.sum + num;
         }
     }
 
-    printf("\nSum of squares of all %d numbers = %lf \n", size, sum_sqrt);
-    printf("\nThere are %d negative numbers. Arithmetic mean of them = %lf \n", size_neg, sum_neg/size_neg);
-    printf("\nThere are %d numbers from <1,10>. Sum of them = %lf \n", size_1_10, sum_1_10);
-    printf("\nThere are %d second number (starting from 1st input). Product of them = %lf \n", size_every_second, product_every_sec);
-    printf("\nThere are %d even number. Sum of them = %lf \n", size_even, sum_even);
-    printf("\nThere are %d odd number. Sum of them = %lf \n", size_odd, sum_odd);
+    printf("\nThere are %d numbers. Sum of their squares = %lf \n", basic.size, basic.sum);
+    printf("\nThere are %d negative numbers. Arithmetic mean of them = %lf \n", negative.size, negative.sum/negative.size);
+    printf("\nThere are %d numbers from <1,10>. Sum of them = %lf \n", basic_1_10.size, basic_1_10.sum);
+    printf("\nThere are %d second number (starting from 1st input). Product of them = %lf \n", every_second.size, every_second.sum);
+    printf("\nThere are %d even number. Sum of them = %lf \n", even.size, even.sum);
+    printf("\nThere are %d odd number. Sum of them = %lf \n", odd.size, odd.sum);
 
     return 0;
 }
@@ -70,4 +83,39 @@ void print_author_info(void) {
     printf("kierunek/semestr: %s\n", STUDY_INFO);
     printf("grupa: %s\n", GROUP);
     printf("program: %s \n", PROGRAM_NAME);
+}
+
+int insert_number(int num, int num1, int num2) {
+        for (;;) {  
+            int rtn = 0;
+            
+            if(num1 == 0 && num2 == 0) {
+                printf("Enter the value of n: ");
+            } else {
+                printf("Enter %d of %d number: ", num1, num2);
+            }
+
+            rtn = scanf ("%d", &num);
+
+            if (rtn == EOF) {
+                return 1;
+            }
+            else if (rtn == 0) {
+                printf(" Błąd: Liczba nie jest numerem rzeczywistym\n");
+                empty_stdin();
+            }
+            else {
+                empty_stdin();
+                break;
+            }
+        }
+
+    return num;
+}
+
+void empty_stdin(void) { 
+    int c = getchar();
+
+    while (c != '\n' && c != EOF)
+        c = getchar();
 }
