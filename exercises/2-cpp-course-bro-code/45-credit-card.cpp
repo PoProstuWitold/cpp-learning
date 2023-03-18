ef5c09b9c00e3e6094b94f28493ae0089a34ac7b
PoProstuWitold <witekzawada@o2.pl>
2023-03-18 22:15:16 +0100

    Bro Code C++ Course, part 3 (Luhn Algorithm)

---
+++ exercises/2-cpp-course-bro-code/45-credit-card.cpp
@@ -0,0 +1,57 @@
+// Luhn Algorithm
+// https://en.wikipedia.org/wiki/Luhn_algorithm
+// --------------
+// 1. Double every second digit from right to left.
+//    If doubled number is 2 digit, split them 
+// 2. Add all single digits from step 1
+// 3. Add all odd numbered digits from right to left
+// 4. Sum results from steps 2 & 3
+// 5. If step 4 is divisible by 10, the card is valid
+
+#include <iostream>
+
+int getDigit(const int number);
+int sumOddDigits(const std::string cardNumber);
+int sumEvenDigits(const std::string cardNumber);
+
+int main() {
+    std::string cardNumber;
+    int result = 0;
+
+    std::cout << "Enter a credit card #: ";
+    std::cin >> cardNumber;
+
+    result = sumEvenDigits(cardNumber) + sumOddDigits(cardNumber);
+
+    if(result % 10 == 0) {
+        std::cout << cardNumber << " is valid";
+    } else {
+        std::cout << cardNumber << " is not valid";
+    }
+
+	return 0;
+}
+
+int getDigit(const int number) {
+    return number % 10 + (number / 10 % 10);
+}
+
+int sumOddDigits(const std::string cardNumber) {
+    int sum = 0;
+
+    for(int i = cardNumber.size() - 1; i >= 0; i-=2) {
+        sum += cardNumber[i] - '0';
+    }
+
+    return sum;
+}
+
+int sumEvenDigits(const std::string cardNumber) {
+    int sum = 0;
+
+    for(int i = cardNumber.size() - 2; i >= 0; i-=2) {
+        sum += getDigit((cardNumber[i] - '0') * 2);
+    }
+
+    return sum;
+}
